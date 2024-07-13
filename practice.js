ef797889a38fb1b30f044c132a51d572c4afa8fa
mohammed kaif <mkaif555123@gmail.com>
2024-07-13 21:54:04 +0500

    make repo

---
+++ practice.js
@@ -0,0 +1,13 @@
+// console.log("run TypeScript Code");
+// function getAr(...num1) {
+//   return num1;
+// }
+// console.log(getAr(300, 433, 443, 644, 246, 34));
+// let arrs = ["kaif" , 5 , true , "typescript"];
+// console.log(arrs);
+// var obj = {
+//     userName: "kaif",
+//     age: 14,
+//     location: "karachi"
+// };
+// console.log(obj);
---
+++ practice.ts
@@ -0,0 +1,15 @@
+// console.log("run TypeScript Code");
+// function getAr(...num1) {
+//   return num1;
+// }
+// console.log(getAr(300, 433, 443, 644, 246, 34));
+
+// let arrs = ["kaif" , 5 , true , "typescript"];
+// console.log(arrs);
+
+const objs = {
+   userName: "kaif",
+   age: 14,
+   location: "karachi"
+}
+console.log(objs);
